f917f868f8826681c451e6654e722531cc3ca066
abdooo2235 <abdalrhmantheiv15@gmail.com>
2025-12-03 21:35:21 +0200

    add message route

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -14,19 +14,47 @@ export const getUsresForSidebar = async (req, res) => {
 
 export const getMessages = async (req, res) => {
   try {
-    const { id:userToChatId } = req.params
-    const senderId = req.user._id;
-    
+    const { id: userToChatId } = req.params
+    const myId = req.user._id;
+
     const messages = await Message.find({
       $or: [
-        { sender: senderId, receiver: userToChatId },
-        { sender: userToChatId, receiver: senderId },
+        { sender: myId, receiver: userToChatId },
+        { sender: userToChatId, receiver: myId },
       ],
     });
-    
+
     res.status(200).json(messages);
   } catch (error) {
     console.error("Error in getMessages:", error.message);
     res.status(500).json({ error: "Failed to fetch users" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+    // todo: realtime functionality => socket.io
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error("Error in sendMessage:", error.message);
+    res.status(500).json({ error: "Failed to send message" });
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectedRoute } from "../middleware/auth.middleware.js";
-import { getUsresForSidebar, getMessages } from "../controllers/message.controller.js";
+import { getUsresForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/users", protectedRoute, getUsresForSidebar)
-router.get("/:id", protectedRoute, getMessages)
+router.get("/users", protectedRoute, getUsresForSidebar);
+router.get("/:id", protectedRoute, getMessages);
+router.post("/send/:id", protectedRoute, sendMessage);
 
 router.post("/", (req, res) => {
     res.json({ message: "Message sent successfully" });
